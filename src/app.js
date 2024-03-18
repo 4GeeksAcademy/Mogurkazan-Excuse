6e7410038af3aedae701fcb0403b859a0491448d
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-03-18 17:00:21 +0000

    improved version

--- src/app.js
+++ src/app.js
@@ -6,28 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  //generar número random
-  function generarNumero() {
-    return Math.floor(Math.random() * 4);
+  //generar la excusa aleatoria
+  function getRandomExcuse() {
+    //Piezas de la excusa
+    const who = ["The dog", "My grandma", "His turtle", "My bird"];
+    const action = ["ate", "peed", "crushed", "broke"];
+    const what = ["my homework", "the keys", "the car", "the bike"];
+    const when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    //generar número random
+    const whoIndex = Math.floor(Math.random() * who.length);
+    const actionIndex = Math.floor(Math.random() * action.length);
+    const whatIndex = Math.floor(Math.random() * what.length);
+    const whenIndex = Math.floor(Math.random() * when.length);
+
+    // Generar excusa completa
+    return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
   }
-  let whoRandom = who[generarNumero()];
-  let actionRandom = action[generarNumero()];
-  let whatRandom = what[generarNumero()];
-  let whenRandom = when[generarNumero()];
-  let excusa =
-    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
-  console.log(excusa);
-  document.getElementById("excuse").innerText = excusa;
-  console.log("Hello Rigo from the console!");
+  const excuseButton = document.getElementById("excuseButton");
+
+  //Enlazar botón con excusa
+  excuseButton.addEventListener("click", function() {
+    // Mostrar la excusa
+    const excuse = getRandomExcuse();
+    document.getElementById("excuse").innerText = excuse;
+  });
 };
